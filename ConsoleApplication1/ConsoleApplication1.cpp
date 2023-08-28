9fce4d0b89a3bb8fa65d55a56d6b7c9c8841dae7
meUser <duartesaoepdrocat1@gmail.com>
2023-08-28 11:53:13 +0100

    built print() function

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,17 @@
 
-#include <iostream>
 #include <conio.h>
+
+void print(const char* s) {
+	for (;s != 0; s++) {
+		_putch(*s);
+	}
+}
 
 int main()
 {
+	const char text[5] = {'E', 'x', 'i', 't', 0};
+	print(text);
 	while (!_kbhit());
-	std::cout << "exit";
 	return 0;
 }
 
